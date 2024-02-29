0b1f5f5c443505590a9846b07f72ad876935b695
TilakKhatri <tilakkhatri0001@gmail.com>
2024-02-29 22:53:59 +0545

    Update collection center location

--- admin_web/src/pages/admin/user/index.tsx
+++ admin_web/src/pages/admin/user/index.tsx
@@ -13,12 +13,12 @@ function User() {
     <>
       <div className="my-12 px-4 md:px-8 xl:px-12 py-4">
         <div className="flex justify-between items-center">
-          <p>User List</p>
+          <p>Collection center Location</p>
           <button
             onClick={() => setIsModalOpen(true)}
             className="w-fit px-4 primary-btn"
           >
-            Add user
+            Add Location
           </button>
         </div>
       </div>
